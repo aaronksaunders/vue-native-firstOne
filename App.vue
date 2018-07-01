82a5f590451a67d7a79599b074a5d5c3af37052d
aaron saunders <aaronksaunders@gmail.com>
2018-07-01 13:03:13 -0400

    adding documentation and android support

--- App.vue
+++ App.vue
@@ -1,7 +1,10 @@
 <template>
   <view class="container">
-    <app-navigation v-if="authorized"></app-navigation>
-    <auth-navigation v-if="!authorized"></auth-navigation>
+    <app-navigation v-if="authCheckComplete && authorized"></app-navigation>
+    <auth-navigation v-if="authCheckComplete && !authorized"></auth-navigation>
+    <view v-if="!authCheckComplete" :style="{flex:1, alignItems : 'center', justifyContent : 'center'}">
+      <nb-text>LOADING ...</nb-text>
+    </view>
   </view>
 </template>
 
@@ -14,6 +17,7 @@
 
   import * as firebase from "firebase";
 
+  // this allows us access to the vuex-store in all of the components
   Vue.prototype.$store = store;
 
   // registering all native-base components to the global scope of the Vue
@@ -24,25 +28,43 @@
   import SignUpVue from "./views/SignUp.vue";
   import SignInVue from "./views/SignIn.vue";
 
+  // ----------------------------------------------------------------------------
+  // THIS IS THE REACT NAVIGATION STUFF THAT IS WAY DIFFERENT FROM THE
+  // VUE-ROUTER WE ALL KNOW AND LOVE
+
+  // the is the navigation stack for the main application after authentication
+  // or account creation is completed. The stackNavigator is the basic master=detail UX
+  // that is seen in most mobile applications
   const AppNavigation = StackNavigator(
     {
+      // Home route will render the component HomeVue
       Home: HomeVue,
+      // Detail route will render the component DetailVue
       Detail: DetailVue
     },
     {
+      // when launching this "Navigator", use the "Home" route
       initialRouteName: "Home"
     }
   );
 
+  // the is the navigation stack for authentication or creating a user.
+  // The stackNavigator is the basic master=detail UX that is seen in most
+  // mobile applications
   const AuthNavigation = StackNavigator({
+    // SignIn route will render the component SignInVue
     SignIn: SignInVue,
+    // SignUp route will render the component SignUpVue
     SignUp: SignUpVue
   });
+
   export default {
+    // bring in the navigation components
     components: { AppNavigation, AuthNavigation },
     data: function() {
       return {
-        authorized: true
+        authorized: false,
+        authCheckComplete: false
       };
     },
     created() {
@@ -51,11 +73,13 @@
       });
       firebase.auth().onAuthStateChanged(user => {
         if (user) {
-         this.$store.dispatch("user/autoSignIn", user);
+          this.$store.dispatch("user/autoSignIn", user);
+          console.log("onAuthStateChanged", user);
           this.authorized = true;
         } else {
           this.authorized = false;
         }
+        this.authCheckComplete = true;
       });
     }
   };
